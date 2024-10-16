072d6f21879f75da996951b93f37bb41e5487cc5
ELSOLRA <bardakasmiljo@gmail.com>
2024-10-16 12:16:37 +0200

    basename version through env.VITE_APP_BASE_URL

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
-import {
-  /* BrowserRouter, */ HashRouter,
-  Route,
-  Routes,
-} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Loading from "./pages/Loading";
 import SetTimerPage from "./pages/setTimerPage";
 import { Provider } from "react-redux";
@@ -14,8 +10,7 @@ import DigitalTimerPage from "./pages/DigitalTimerPage";
 function App() {
   return (
     <Provider store={store}>
-      {/* <BrowserRouter> */}
-      <HashRouter>
+      <BrowserRouter basename={import.meta.env.VITE_APP_BASE_URL}>
         <Routes>
           <Route path="/" element={<Loading />} />
           <Route path="/set-timer" element={<SetTimerPage />} />
@@ -23,10 +18,30 @@ function App() {
           <Route path="/digital" element={<DigitalTimerPage />} />
           <Route path="/break" element={<BreakPage />} />
         </Routes>
-        {/* </BrowserRouter> */}
-      </HashRouter>
+      </BrowserRouter>
     </Provider>
   );
 }
+
+// other method how to do basname!
+/*     <BrowserRouter basename={import.meta.env.DEV ? "/" : "/timer/"}>
+
+ 
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+ 
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: "/",
+  };
+ 
+  if (command !== "serve") {
+    config.base = "/timer/";
+  }
+ 
+  return config;
+}); */
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Interval-app-reactJs/",
+  base: loadEnv(mode, process.cwd(), "").VITE_APP_BASE_URL,
   server: {
     watch: {
       usePolling: true,
