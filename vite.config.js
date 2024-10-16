6c894eaba4b49b4e1f6431a7a2235bb6b87048dd
ELSOLRA <bardakasmiljo@gmail.com>
2024-10-16 12:27:31 +0200

    correction for env.VITE_APP_BASE_URL method

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,20 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: loadEnv(mode, process.cwd(), "").VITE_APP_BASE_URL,
-  server: {
-    watch: {
-      usePolling: true,
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: loadEnv(mode, process.cwd(), "").VITE_APP_BASE_URL,
+    server: {
+      watch: {
+        usePolling: true,
+      },
+      host: true,
+      strictPort: true,
+      port: 5173,
     },
-    host: true,
-    strictPort: true,
-    port: 5173,
-  },
-  css: {
-    postcss: "./postcss.config.js",
-  },
+    css: {
+      postcss: "./postcss.config.js",
+    },
+  };
 });
